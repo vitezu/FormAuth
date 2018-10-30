24a0bf7f531b1e34ca2750f86b7ee28498501ab1
Pavel_Z <zubrytski.pavel@gmail.com>
2018-10-30 16:13:26 +0300

    add

---
+++ FormAuth/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FormAuth.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+        base("DefaultConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
